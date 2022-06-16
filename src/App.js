34db418d5c28e271d43ca4d7886084050efaae3b
Akanimo Sam Okure <akan.okure@gmail.com>
2022-06-16 16:26:48 +0100

    add and implement API CRUD using fetch api and react table to deplay response data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import "./App.css";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Carlist from "./components/Carlist";
 
+// eslint-disable-next-line no-undef
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header"></header>
-        <h1 className="App-title">CarList</h1>
+        <header className="App-header">
+          <h1 className="App-title">CarList</h1>
+        </header>
+        <Carlist />
       </div>
     );
   }
